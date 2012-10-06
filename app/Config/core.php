1e0bce83752fa96d456932934f9cfff5185ff142
web developer <iprathik@gmail.com>
2012-10-06 23:20:01 +0530

    Prefix change

--- app/Config/core.php
+++ app/Config/core.php
@@ -259,7 +259,7 @@ if (Configure::read('debug') >= 1) {
 }
 
 // Prefix each application on the same server with a different string, to avoid Memcache and APC conflicts.
-$prefix = 're_';
+$prefix = 'cb_';
 
 /**
  * Configure the cache used for general framework caching.  Path information,
